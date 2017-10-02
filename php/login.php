795d03bebe4340fb4b3258be76c44ba995594c0e
CodeBackpackofEndika <32270483+CodeBackpackofEndika@users.noreply.github.com>
2017-10-02 12:39:47 +0200

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,16 @@
+
+
+
+
+<?php
+
+
+echo "<H2><u><strong>User:</strong></u></H2>";
+echo $_POST["user"];
+echo "<br></br>";
+echo "<H2><u><strong>Password:</strong></u></H2>";
+echo $_POST["password"];
+
+?>
+
+
